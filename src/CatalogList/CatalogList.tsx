e9071bb2de7ab549722bf53eab2eba014345ee3e
maniek <maniek9274@gmail.com>
2026-06-05 20:45:03 +0200

    Add onClick of list in CatalogList.tsx

--- src/CatalogList/CatalogList.tsx
+++ src/CatalogList/CatalogList.tsx
@@ -2,15 +2,16 @@ import { type Planets} from "../data.ts";
 
 interface CatalogListProps {
     objects: Planets[];
+    onChange: (id: number) => void;
 }
 function CatalogList(props: CatalogListProps) {
     return (
         <>
             <div className="Catalog">
                 <h2>Katalog</h2>
-
             {props.objects.map((obj) => (
-                <div key={obj.id} className="Elements">
+                <div key={obj.id} className="Elements"
+                    onClick={() => props.onChange(obj.id)}>
                     <p>{obj.name}</p>
                 </div>
             ))}
