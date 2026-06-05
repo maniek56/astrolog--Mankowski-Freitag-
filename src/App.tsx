75352bde1c5f0976cb8a96968aeb8343b3684e8c
maniek <maniek9274@gmail.com>
2026-06-05 19:18:05 +0200

    Add photos of planets and header

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,10 @@ import './App.css'
 function App() {
   return (
     <>
-
+     <div className="App">
+       <h1>AstroLog</h1>
+     </div>
     </>
   )
 }
-
 export default App
---
+++ src/IntroScreen/IntroScreen.tsx
@@ -0,0 +1,8 @@
+function IntroScreen() {
+    return (
+        <>
+
+        </>
+    )
+}
+export default IntroScreen
