2349483ce1eaf9edb33f851666694a7f6db8954a
maniek <maniek9274@gmail.com>
2026-06-05 21:18:34 +0200

    Add skeleton in DetailsScreen.tsx and update data.ts

--- src/DetailsScreen/DetailsScreen.tsx
+++ src/DetailsScreen/DetailsScreen.tsx
@@ -1,7 +1,25 @@
-function DetailsScreen() {
+import { type Planets } from "../data.ts";
+
+interface DetailsScreenProps {
+    objects: Planets[];
+}
+
+function DetailsScreen(props: DetailsScreenProps) {
     return (
         <>
+            {props.objects.map((obj) => (
+                <div key={obj.id} className="details">
+                    <div className="photo">
+                        <img src={obj.photo} alt={obj.name} width={200} />
+                    </div>
+                    <div className="description">
+                        <h3>{obj.name}</h3>
+                        <p>Typ: {obj.type}</p>
+                        <p>Odległość: {obj.distanceFromEarth}</p>
+                    </div>
+                </div>
+            ))}
         </>
-    )
+    );
 }
-export default DetailsScreen
+export default DetailsScreen;
--- src/data.ts
+++ src/data.ts
@@ -7,12 +7,14 @@ import uran from "./assets/uran.png";
 export interface Planets {
     id: number;
     name: string;
+    type: string;
+    distanceFromEarth: string;
     photo: string;
 }
 export const planets: Planets[] = [
-    { id:  1, name: "Merkury", photo: merkury, },
-    { id:  2, name: "Jowisz", photo: jowisz, },
-    { id:  3, name: "Saturn", photo: saturn, },
-    { id:  4, name: "Mars", photo: mars, },
-    { id:  5, name: "Uran", photo: uran, },
+    { id:  1, name: "Merkury", type: "Planeta", distanceFromEarth: "test", photo: merkury, },
+    { id:  2, name: "Jowisz", type: "Planeta", distanceFromEarth: "test", photo: jowisz, },
+    { id:  3, name: "Saturn", type: "Planeta", distanceFromEarth: "test", photo: saturn, },
+    { id:  4, name: "Mars", type: "Planeta", distanceFromEarth: "test", photo: mars, },
+    { id:  5, name: "Uran", type: "Planeta", distanceFromEarth: "test", photo: uran, },
 ]
