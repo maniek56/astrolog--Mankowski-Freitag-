e745b5ac2f1c60e49899fd4237d76dfcce7c1af9
maniek <maniek9274@gmail.com>
2026-06-05 20:10:48 +0200

    Add useState of planets and testing display of photos

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import './App.css'
-import { planets, Planets } from "./assets/data.ts";
+import { planets, type Planets } from "./data.ts";
+import {useState} from "react";
 
 function App() {
+  const [objects] = useState<Planets[]>(planets);
+  const photosTest = objects.map((obj: Planets) => obj.photo);
+
   return (
     <>
      <div className="App">
        <h1>AstroLog</h1>
+       {photosTest.map((photo, index) => (
+           <img key={index} src={photo}  width="100"/>
+       ))}
      </div>
     </>
   )
 }
+
 export default App
