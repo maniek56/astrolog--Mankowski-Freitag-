07e4c977a9f2744e7e0682995c976d176b8ad11d
maniek <maniek9274@gmail.com>
2026-06-07 14:52:15 +0200

    Add directionFromEarth on the data.ts

--- src/data.ts
+++ src/data.ts
@@ -12,9 +12,9 @@ export interface Planets {
     photo: string;
 }
 export const planets: Planets[] = [
-    { id:  1, name: "Merkury", type: "Planeta", distanceFromEarth: "test", photo: merkury, },
-    { id:  2, name: "Jowisz", type: "Planeta", distanceFromEarth: "test", photo: jowisz, },
-    { id:  3, name: "Saturn", type: "Planeta", distanceFromEarth: "test", photo: saturn, },
-    { id:  4, name: "Mars", type: "Planeta", distanceFromEarth: "test", photo: mars, },
-    { id:  5, name: "Uran", type: "Planeta", distanceFromEarth: "test", photo: uran, },
+    { id:  1, name: "Merkury", type: "Planeta", distanceFromEarth: "77 000 000 km", photo: merkury, },
+    { id:  2, name: "Jowisz", type: "Planeta", distanceFromEarth: "778 500 000 km", photo: jowisz, },
+    { id:  3, name: "Saturn", type: "Planeta", distanceFromEarth: "1 200 000 000 km", photo: saturn, },
+    { id:  4, name: "Mars", type: "Planeta", distanceFromEarth: "54 600 000 km", photo: mars, },
+    { id:  5, name: "Uran", type: "Planeta", distanceFromEarth: "2 600 000 000 km", photo: uran, },
 ]
