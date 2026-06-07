7383a86dbac420c2b54df64c9210212ca0ce2cf5
maniek <maniek9274@gmail.com>
2026-06-07 22:45:20 +0200

    Fix displaying of paragraph in IntroScreen.tsx

--- src/IntroScreen/IntroScreen.tsx
+++ src/IntroScreen/IntroScreen.tsx
@@ -7,7 +7,7 @@ function IntroScreen(props: IntroScreenProps) {
         <>
             <div className="intro">
                 <h2>Witaj w AstroLog</h2>
-                <p>Astrolog to katalog ciał niebieskich naszego obserwatorium. </p>
+                <p>Astrolog to katalog ciał niebieskich naszego obserwatorium.</p>
                 <p>Wybierz obiekt z listy aby zobaczyć jego dane.</p>
                 <p>Jeśli odkryłeś nowy obiekt, możesz go zgłosić w Rejstrze odkryć</p>
                 <button onClick={props.onOpenForm}>Rejestr odkryć</button>
