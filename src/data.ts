d9af44b6ac941a7bf45059e03a78c9e13ad3b946
maniek <maniek9274@gmail.com>
2026-06-07 15:22:21 +0200

    Update directionFromEarth on the data.ts

--- src/data.ts
+++ src/data.ts
@@ -12,9 +12,9 @@ export interface Planets {
     photo: string;
 }
 export const planets: Planets[] = [
-    { id:  1, name: "Merkury", type: "Planeta", distanceFromEarth: "77 000 000 km", photo: merkury, },
+    { id:  1, name: "Merkury", type: "Planeta", distanceFromEarth: "91 000 000 km", photo: merkury, },
     { id:  2, name: "Jowisz", type: "Planeta", distanceFromEarth: "778 500 000 km", photo: jowisz, },
-    { id:  3, name: "Saturn", type: "Planeta", distanceFromEarth: "1 200 000 000 km", photo: saturn, },
-    { id:  4, name: "Mars", type: "Planeta", distanceFromEarth: "54 600 000 km", photo: mars, },
-    { id:  5, name: "Uran", type: "Planeta", distanceFromEarth: "2 600 000 000 km", photo: uran, },
+    { id:  3, name: "Saturn", type: "Planeta", distanceFromEarth: "1 275 000 000 km", photo: saturn, },
+    { id:  4, name: "Mars", type: "Planeta", distanceFromEarth: "225 000 000 km", photo: mars, },
+    { id:  5, name: "Uran", type: "Planeta", distanceFromEarth: "2 870 000 000 km", photo: uran, },
 ]
